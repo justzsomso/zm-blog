439f3e08b55a0cc26bac21e8f27359c06fa7b5fd
Justzsomso <Justzsomso@126.com>
2023-01-11 14:53:17 +0800

    登录认证过滤器功能编写

--- zhoumin-blog/src/main/java/com/zhoumin/config/SecurityConfig.java
+++ zhoumin-blog/src/main/java/com/zhoumin/config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.zhoumin.config;
 
+import com.zhoumin.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -33,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         http.logout().disable();
+
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         //允许跨域
         http.cors();
     }
---
+++ zhoumin-blog/src/main/java/com/zhoumin/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,75 @@
+package com.zhoumin.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.zhoumin.domain.ResponseResult;
+import com.zhoumin.domain.entity.LoginUser;
+import com.zhoumin.enums.AppHttpCodeEnum;
+import com.zhoumin.utils.JwtUtil;
+import com.zhoumin.utils.RedisCache;
+import com.zhoumin.utils.WebUtils;
+import io.jsonwebtoken.Claims;
+import jdk.nashorn.internal.ir.IfNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //获取请求头中的token
+        String token = request.getHeader("token");
+        if (StringUtils.hasText(token)) {
+            //这个接口不需要token，直接放行
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //解析获取userId
+        Claims claims = null;
+        try {
+            claims = JwtUtil.parseJWT(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //token超时 或者 token被篡改非法
+
+            //响应前端需要重新登录
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+
+            WebUtils.renderString(response, JSON.toJSONString(result));
+            return;
+        }
+        String userId = claims.getSubject();
+        //从redis中获取用户信息
+        LoginUser loginUser = redisCache.getCacheObject("bloglogin:" + userId);
+
+        //如果获取不到loginUser
+        if (Objects.isNull(loginUser)) {
+            //登录过期
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+
+            WebUtils.renderString(response, JSON.toJSONString(result));
+            return;
+        }
+
+        //存入SecurityContextHolder
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser,null,null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        filterChain.doFilter(request,response);
+
+    }
+
+}
