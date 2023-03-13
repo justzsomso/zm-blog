98ae0acf54beaf8dd24f56c73ba42cf010b144d8
Justzsomso <Justzsomso@126.com>
2023-03-13 23:13:08 +0800

    发送评论接口代码实现 47/155

--- zhoumin-blog/src/main/java/com/zhoumin/controller/CommentController.java
+++ zhoumin-blog/src/main/java/com/zhoumin/controller/CommentController.java
@@ -1,11 +1,10 @@
 package com.zhoumin.controller;
 
 import com.zhoumin.domain.ResponseResult;
+import com.zhoumin.domain.entity.Comment;
 import com.zhoumin.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/comment")
@@ -18,5 +17,11 @@ public class CommentController {
     public ResponseResult commentList(Long articleId, Integer pageNum, Integer pageSize){
         return commentService.commentList(articleId,pageNum,pageSize);
     }
+
+    @PostMapping
+    public ResponseResult addComment(@RequestBody Comment comment,@RequestHeader(value = "token",required = true ) String token){
+        return commentService.addComment(comment,token);
+    }
+
 
 }
